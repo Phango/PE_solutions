f11ac00af6d6c5a2d4e1749de6a8172cc06dc3e5
Michael Pang <michaelpang@Michaels-MacBook-Pro.local>
2018-12-25 00:36:07 +0100

    js solution for problem 6

---
+++ js/6.js
@@ -0,0 +1,25 @@
+var common = require("./Common");
+
+function answer(upperLimit) {
+  var range = common.getRange(1, upperLimit);
+  // console.log(range)
+
+  var sumOfSquares = range.map(function(value) {
+    return value ** 2;
+  }).reduce(function(prev, curr) {
+    return prev + curr;
+  });
+
+  var squareOfSum = range.reduce(function(prev, curr) {
+    return prev + curr;
+  }) ** 2;
+
+  return squareOfSum -  sumOfSquares;
+}
+
+function square(a) {
+  return a**2;
+}
+
+// console.log(answer(10));
+console.log(answer(100));
